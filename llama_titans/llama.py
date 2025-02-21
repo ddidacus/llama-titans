71c6f8562166fc2af767f9185f865c3162cf9093
ddidacus <diego.calanzone@mila.quebec>
2025-02-21 18:26:01 -0500

    MAG/MAL switch; added reset memory toggle for high-level memory states

--- llama_titans/llama.py
+++ llama_titans/llama.py
@@ -11,28 +11,44 @@ from transformers.models.llama.modeling_llama import LlamaForCausalLM
 from transformers.models.llama.modeling_llama import LlamaDecoderLayer
 from transformers.models.llama.configuration_llama import LlamaConfig
 
+# https://github.com/lucidrains/titans-pytorch/blob/55b4d712eae34ad7c593da0beb7e33a7f9c2e43a/titans_pytorch/mac_transformer.py#L776
+# TODO/Questions:
+#   - should the memory state be passed across layers as in the original implementations?
+#   - or does it work also the way it's implemented here: independent memories by layer (abstraction), passing state over time
+
 class TitanLlamaModel(LlamaForCausalLM):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, gated:bool=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        if hasattr(args, 'gated'): self.gated = gated
+        else: self.gated = False
+
+        # Applying titan layer to llama
         for idx, layer in enumerate(self.model.layers):
             if isinstance(layer, LlamaDecoderLayer):
                 new_layer = TitanLlamaDecoderLayer(config=self.config, layer_idx=idx).to(self.device)
                 new_layer.load_state_dict(layer.state_dict(), strict=False)
                 self.model.layers[idx] = new_layer
                 del layer
-    
+
+    def reset_memory_states(self):
+        for idx in range(len(self.model.layers)):
+            if isinstance(self.model.layers[idx], TitanLlamaDecoderLayer):
+                self.model.layers[idx].memory_state = None
+                
     @staticmethod
-    def from_pretrained(path: str):
+    def from_pretrained(path: str, gated:bool = False):
         config = LlamaConfig.from_pretrained(path)
         state_dict = LlamaForCausalLM.from_pretrained(path).state_dict()
-        model = TitanLlamaModel(config=config)
+        model = TitanLlamaModel(config=config, gated=gated)
         model.load_state_dict(state_dict, strict=False)
         del state_dict
         return model
 
 class TitanLlamaDecoderLayer(nn.Module):
-    def __init__(self, config: LlamaConfig, layer_idx: int):
+    def __init__(self, config: LlamaConfig, layer_idx: int, gated:bool = False):
         super().__init__()
+        self.gated = gated
         self.hidden_size = config.hidden_size
         if hasattr(config, "chunk_size"): self.chunk_size = config.chunk_size
         else: self.chunk_size = 64
@@ -62,10 +78,9 @@ class TitanLlamaDecoderLayer(nn.Module):
     ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
         residual = hidden_states
 
-        # Memory as Layer (MAL)
         hidden_states, new_state = self.memory(seq=hidden_states, state=self.memory_state)
         self.memory_state = new_state
-
+        
         hidden_states = self.input_layernorm(hidden_states)
 
         # Self Attention
@@ -80,6 +95,11 @@ class TitanLlamaDecoderLayer(nn.Module):
             position_embeddings=position_embeddings,
             **kwargs,
         )
+        # Memory As Gate (MAG), otherwise Memory As Layer (MAL)
+        if self.gated: 
+            attn_out_gates = hidden_states.sigmoid()
+            hidden_states *= attn_out_gates
+
         hidden_states = residual + hidden_states
 
         # Fully Connected
